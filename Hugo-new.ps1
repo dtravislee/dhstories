310938eab375e8340660a884a925a4426d991002
D. Travis Lee <49222803+dtravislee@users.noreply.github.com>
2024-05-09 13:29:22 -0700

    Update Hugo-new.ps1
    Updated for new tree structure

--- Hugo-new.ps1
+++ Hugo-new.ps1
@@ -2,6 +2,6 @@ $ScriptPath = Split-Path -Parent $Script:MyInvocation.MyCommand.Path
 cd $ScriptPath/dtravislee-website
 $FileName = Read-Host -prompt "Enter filename"
 $FileName = $FileName -replace '\s','-'
-hugo new other/$FileName.md
+hugo new posts/$FileName.md
 & Start notepad++ content/posts/$FileName.md
 cmd /c pause
